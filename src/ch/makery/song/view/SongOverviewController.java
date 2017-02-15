3fdcd386ab11b20ad5562da68b238aede1069ceb
NullTerra180 <nullterra180@gmail.com>
2017-02-14 22:07:24 -0500

    fixes-03
    added Robet's code; sorting functionality needs to be fixed

--- src/ch/makery/song/view/SongOverviewController.java
+++ src/ch/makery/song/view/SongOverviewController.java
@@ -1,15 +1,24 @@
 package ch.makery.song.view;
 
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.util.Callback;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import ch.makery.song.SongLib;
 import ch.makery.song.model.Song;
 
 public class SongOverviewController {
+
+	int numSongs = 6;
+	int currentSong;
 	/*
 	 * All private fields and methods where the fxml file
 	 * needs access must be annotated with "@FXML".
@@ -47,10 +56,9 @@ public class SongOverviewController {
 		//Initialize the song table with the column.
 		songTitleColumn.setCellValueFactory(cellData -> cellData.getValue().songTitleProperty());
 
-		//Preselect the first song in the column.
-
 		//Clear song details.
 		showSongDetails(null);
+		currentSong = 0;
 
 		//Listen for selection changes and show the person details when changed.
 		songTable.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> showSongDetails(newValue));
@@ -63,6 +71,31 @@ public class SongOverviewController {
 	 */
 	public void setSongLib(SongLib songLib) {
 		this.songLib = songLib;
+
+	//	Callback<Song,Observable[]> cb =(Song song) -> new Observable[]{
+	//	        song.songTitleProperty(),
+	//	    };
+
+		// 3. Wrap the observable list in a SortedList.
+       SortedList<Song> sortedData = new SortedList<>(songLib.getSongData(),
+       (Song song1, Song song2) -> {
+    	   if((song1.getSongTitle()).compareTo(song2.getSongTitle()) < 0){
+    		   return -1;
+    	   } else if ((song1.getSongTitle()).compareTo(song2.getSongTitle()) > 0 ){
+    		   return 1;
+    	   } else {
+    		   return 0;
+    	   }
+       });
+
+	  	// 4. Bind the SortedList comparator to the TableView comparator.
+       	//sortedData.comparatorProperty().bind(songTable.comparatorProperty());
+
+       	//Add observable list data to the table.
+		songTable.setItems(sortedData);
+
+		songTable.getSelectionModel().select(currentSong);
+		songTable.getFocusModel().focus(currentSong);
 
 		//Add observable list data to the table.
 		songTable.setItems(songLib.getSongData());
@@ -90,7 +123,18 @@ public class SongOverviewController {
 	private void handleDeleteSong() {
 		int selectedIndex = songTable.getSelectionModel().getSelectedIndex();
 		if(selectedIndex >= 0) {
+			currentSong = selectedIndex;
 			songTable.getItems().remove(selectedIndex);
+			numSongs--;
+			if (numSongs > 0){
+				if (currentSong + 1 >= numSongs){
+					songTable.getSelectionModel().select(numSongs - 1);
+					songTable.getFocusModel().focus(numSongs - 1);
+				} else {
+				songTable.getSelectionModel().select(currentSong);
+				songTable.getFocusModel().focus(currentSong);
+				}
+			} else return;
 		} else {
 			//No song selected.
 			Alert noSongAlert = new Alert(AlertType.WARNING);
@@ -112,6 +156,7 @@ public class SongOverviewController {
 		boolean okClicked = songLib.showSongEditDialog(tempSong);
 		if(okClicked) {
 			songLib.getSongData().add(tempSong);
+			numSongs++;
 		}
 	}
 
